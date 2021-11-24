d60e5844b44ea82e6d1fbc933a17f06a4985dae3
Antoine Vignon <vignonantoinem@gmail.com>
2021-11-24 03:23:19 -0500

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(compression()); //Compress all routes
 app.use(helmet());
-//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'routes')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
